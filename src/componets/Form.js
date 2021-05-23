6d87ad9e6457330b877a141f2ca309645cf7d597
yuu0305 <segyayuu8@gmail.com>
2021-05-23 23:52:58 +0900

    Change code in Form.js
    Refactoring Form code

--- src/componets/Form.js
+++ src/componets/Form.js
@@ -1,8 +1,8 @@
 const Form = ({setCity,getWeather}) => {
     return (
-        <form>
+        <form onSubmit={getWeather}>
             <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <button type="submit">Get Weather</button>
         </form>
     );
 };
