e093221d7b94b9274ed0dbdb037b3fc280d437f1
yuu0305 <segyayuu8@gmail.com>
2021-05-23 23:37:22 +0900

    ref
    Code refactorig

--- src/componets/Form.js
+++ src/componets/Form.js
@@ -1,8 +1,8 @@
-const Form = (props) => {
+const Form = ({setCity,getWeather}) => {
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
-            <button type="submit" onClick={props.getWeather}>Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
